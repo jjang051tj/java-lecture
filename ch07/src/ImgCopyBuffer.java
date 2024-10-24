f84e31c16904ba0eebfc122e8d2f1bb73b94b087
jjang051tj <jjang051tj@gmail.com>
2024-10-24 15:09:32 +0900

    img copy

--- ch07/src/ImgCopyBuffer.java
+++ ch07/src/ImgCopyBuffer.java
@@ -1,18 +1,23 @@
 import java.io.*;
 
-public class ImgCopy {
+public class ImgCopyBuffer {
     public static void main(String[] args) {
-        //파일 / 폴더
         File sourceFile =  new File("temp/jun.jpg");
-        File copyFile = new File("temp/copy-jun.jpg");
+        File copyFile = new File("temp/copy-jun-buffer.jpg");
+
         try {
+
             FileInputStream fileInputStream = new FileInputStream(sourceFile);
             FileOutputStream fileOutputStream = new FileOutputStream(copyFile);
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream,1024*8);
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream,1024*8);
             long startTime = System.currentTimeMillis(); //mill
             int c = 0;
-            while((c = fileInputStream.read())!=-1) {
-                fileOutputStream.write((byte)c);
+            while((c = bufferedInputStream.read())!=-1) {
+                bufferedOutputStream.write((byte)c);
             }
+            bufferedInputStream.close();
+            bufferedOutputStream.close();
             fileInputStream.close();
             fileOutputStream.close();
             long endTime = System.currentTimeMillis();
