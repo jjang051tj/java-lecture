bf2e087ef2e8b98a5cc8bc164dab96b1811dd557
jjang051tj <jjang051tj@gmail.com>
2024-10-24 11:25:12 +0900

    문자 인코딩

--- ch07/src/FileReaderTest.java
+++ ch07/src/FileReaderTest.java
@@ -3,26 +3,24 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class FileReaderTest {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         //외부에 있는 파일들은
         //오류 잡기 try catch
+        // 오류 발생
+
+        //컴파일 오류  실행전에 미리 알려진 오류 실행 자체가 안됨
+        //런타임 오류  실행을 하고나서야 알게되는 오류
         try {
-            //파일이 정해진 경로에 있다.
-            FileReader fileReader = new FileReader("temp/system.ini");
             int c;
-
-
-//            while((c = fileReader.read())!=-1) {
-//                System.out.print((char)c);
-//            }
+            FileReader fileReader = new FileReader("temp/sample.txt");
             while(true) {
                 c = fileReader.read();
                 if(c==-1) break;
                 System.out.print((char)c);
             }
         } catch (FileNotFoundException e) {
-            System.out.println("파일이 경로에 없습니다.");
             throw new RuntimeException(e);
+            // 프로그램이 종료되지는 않는다.
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
