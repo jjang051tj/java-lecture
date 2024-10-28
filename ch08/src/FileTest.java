1632d354d5a77a520c1029e11b86616e5c18eb5b
jjang051tj <jjang051tj@gmail.com>
2024-10-28 10:11:04 +0900

    파일 다루기

--- ch08/src/FileTest.java
+++ ch08/src/FileTest.java
@@ -7,11 +7,28 @@ public class FileTest {
         //폴더 만들기.
         File file = new File("temp/example.txt");
         File directory = new File("temp/exampleDir");
+
+        System.out.println("is File : "+file.isFile());
+        System.out.println("is File : "+directory.isFile());
+
+        System.out.println("is File : "+file.isDirectory());
+        System.out.println("is File : "+directory.isDirectory());
+
+        // profile.jpg   profile.jpg
+        System.out.println("File name : "+file.getName());
+        System.out.println("File 크기 : "+file.length());
+
+        File newFile = new File("temp/newExample.txt");
+        boolean isRenamed = file.renameTo(newFile);
+        System.out.println("isRenamed==="+isRenamed);
+
+        //파일이 복사되고 이름이 바뀌어서 저장된다.
         if(!directory.exists()) {
             directory.mkdir();
         }
         if(file.exists()) {
             System.out.println("temp/example.txt이 있습니다.");
+            file.delete();
         } else {
             try {
                 boolean isCreate = file.createNewFile();
